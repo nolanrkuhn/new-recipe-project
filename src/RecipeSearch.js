20cf2e1a25affcb0152d13f90cae3f96e1e38f69
nolanrkuhn <nolanrkuhn@gmail.com>
2025-02-12 13:21:23 -0700

    done

--- src/RecipeSearch.js
+++ src/RecipeSearch.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import config from '../config';
+import config from './config';
 
 const RecipeSearch = ({ user }) => {
     const [query, setQuery] = useState('');
